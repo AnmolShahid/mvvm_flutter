949ee43f000e3e1a2685d07c40b5d77bab2e400e
anmolshahhid@hotmail.com <anmolshahid@hotmail.com>
2022-09-03 05:44:38 +1000

    commit

---
+++ lib/domain/usecase/forgot_password_usecase.dart
@@ -0,0 +1,15 @@
+import 'package:sample_flutter/data/network/failure.dart';
+import 'package:sample_flutter/domain/repository/repository.dart';
+import 'package:sample_flutter/domain/usecase/base_usecase.dart';
+import 'package:dartz/dartz.dart';
+
+class ForgotPasswordUseCase implements BaseUseCase<String, String> {
+  final Repository _repository;
+
+  ForgotPasswordUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, String>> execute(String input) async {
+    return await _repository.forgotPassword(input);
+  }
+}
